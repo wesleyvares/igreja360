012ba4dfa49c1c06d5ebbb6e38fac8d2bc3e600d
Wesley <135629962+wesleyvares@users.noreply.github.com>
2026-09-24 00:06:07 -0300

    chore: aceitar variáveis intencionalmente ignoradas no lint

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,8 @@ export default [
   {
     files: ['**/*.{ts,tsx}'],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off'
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
     }
   }
 ];
