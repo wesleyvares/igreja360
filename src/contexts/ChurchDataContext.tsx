a50ac57cf4788680fb841beb2c78eb02e60bca5a
Wesley <135629962+wesleyvares@users.noreply.github.com>
2026-09-24 00:06:36 -0300

    chore: remover diretiva obsoleta de react-hooks

--- src/contexts/ChurchDataContext.tsx
+++ src/contexts/ChurchDataContext.tsx
@@ -139,7 +139,6 @@ export function ChurchDataProvider({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     refresh();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user?.uid, user?.celulaId]);
 
   function getStateSetter(collection: CollectionName) {
